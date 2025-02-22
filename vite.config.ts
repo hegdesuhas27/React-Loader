b3abe03e78ed283c5f33e17890fa16a45bbe9433
Suhas Hegde <hegdesuhas27@gmail.com>
2025-02-22 23:52:27 +0530

    Add build files

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/React-Loader/',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      input: resolve(__dirname, 'index.html'),
-    },
+    outDir: '.',
+    emptyOutDir: false,
   },
 })
