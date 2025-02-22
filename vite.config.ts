10a556cab1758750525400b75d6029db121e80f9
Suhas Hegde <hegdesuhas27@gmail.com>
2025-02-22 23:36:51 +0530

    chore: build configs

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/loader-component-task/',
+  base: '/React-Loader/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
 })
